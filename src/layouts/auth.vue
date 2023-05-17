f921f80543fa791e214a1cb8cf43ea46b9351a77
Duy Dat <duydat30122002@gmail.com>
2023-05-17 20:45:05 +0700

    Init + Add Router, Layout

---
+++ src/layouts/auth.vue
@@ -0,0 +1,10 @@
+<template>
+  <h1>Auth layout</h1>
+  <slot />
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
---
+++ src/layouts/default.vue
@@ -0,0 +1,10 @@
+<template>
+  <h1>Default layout</h1>
+  <slot />
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
